c67064e89173d2ac857e504c70260ca702945a6e
ferid0t <ferido@192.168.1.134>
2026-03-30 23:54:16 +0300

    publications guncellendi

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -88,13 +88,123 @@ export const publications = [
 		time: 'November 01 2024',
 		link: 'https://link.springer.com/article/10.1007/s11694-024-02927-7'
 	
-	}
-	// {
-	// 	title: 'The Radiation of Uranium Compounds',
-	// 	authors: 'Marie Curie',
-	// 	journal: 'Comptes Rendus de l’Académie des Sciences',
-	// 	time: '1898',
-	// 	link: '#',
-	// 	abstract: 'Early research leading to the identification of uranium’s radioactive properties.',
-	// },
+	},
+	{
+	title: 'Nanoliposomal Encapsulation and Purification of Angiotensin-Converting Enzyme Inhibitor Peptides from Ulva rigida',
+	authors: 'Eda Şensu, Harun Koku, Evren Demircan, Sebahat Şişman, İbrahim Gülseren, Tuğçe Karaduman, Bilal Çakır, Emine Şükran Okudan, Gökhan Duruksu, Beraat Özçelik, Aysun Yücetepe',
+	journal: 'ACS Omega',
+	time: 'May 2025',
+	link: 'https://doi.org/10.1021/acsomega.5c00780'
+	},
+	{
+	title: 'Macroalgae-derived hydrocolloids and their applications in food industry and their health effects',
+	authors: 'Gizem Kezer, Büşra Yusupoğlu, Serap Namlı, Sara Darabidarestani, Iskandar Azmy Harahap, Eda Şensu, Esha Trivedi, Aysun Yücetepe, Zyta M. Ziora, Tuba Esatbeyoglu',
+	journal: 'European Food Research and Technology',
+	time: 'March 2026',
+	link: 'https://doi.org/10.1007/s00217-026-05084-x'
+	},
+
+	{
+	title: 'Macroalgae-derived hydrocolloids and their applications in food industry and their health effects',
+	authors: 'Gizem Kezer, Büşra Yusupoğlu, Serap Namlı, Sara Darabidarestani, Iskandar Azmy Harahap, Eda Şensu, Esha Trivedi, Aysun Yücetepe, Zyta M. Ziora, Tuba Esatbeyoglu',
+	journal: 'European Food Research and Technology',
+	time: 'March 2026',
+	link: 'https://doi.org/10.1007/s00217-026-05084-x'
+	},
+
+	{
+	title: 'Micro-encapsulation exhibits better protection than nano-encapsulation on phenolics before and after in vitro digestion',
+	authors: 'Eda Şensu, Ümit Altuntaş, Mine Gültekin-Özgüven, Ayhan Duran, Beraat Özçelik, Aysun Yücetepe',
+	journal: 'Food Bioscience',
+	time: 'December 2024',
+	link: 'https://doi.org/10.1016/j.fbio.2024.105159'
+},
+
+	{
+	title: 'Alternatif protein kaynağı olarak makroalgler ve makroalgal proteinlerin ekstraksiyonu: bir derleme çalışması',
+	authors: 'Eda Şensu, Aysun Yücetepe, Beraat Özçelik',
+	journal: 'GIDA - The Journal of Food',
+	time: '2024',
+	link: 'https://doi.org/10.15237/gida.GD24093'
+},
+
+{
+	title: 'Optimization of Protein Extraction from Halopteris scoparia Macroalgae by Ultrasonic-Assisted Enzymatic Extraction (UAEE): Bioactive, Chemical, and Technological Properties',
+	authors: 'Ayşegül Bozdemir, Eda Şensu, Mine Gültekin-Özgüven, Emine Şükran Okudan, Aysun Yücetepe',
+	journal: 'ACS Food Science & Technology',
+	time: 'May 2024',
+	link: 'https://doi.org/10.1021/acsfoodscitech.4c00032'
+},
+
+{
+	title: 'ULTRASOUND-ASSISTED ENZYMATIC EXTRACTION OF ANTIOXIDATIVE PROTEIN EXTRACTS FROM SARGASSUM VULGARE: OPTIMIZATION OF EXTRACTION PARAMETERS USING RSM',
+	authors: 'Eda Şensu, Hatice Dinç, Emine Şükran Okudan, Aysun Yücetepe',
+	journal: 'GIDA - The Journal of Food',
+	time: '2024',
+	link: 'https://doi.org/10.15237/gida.GD23141'
+},
+
+{
+	title: 'Characterization of Proteins Extracted from Ulva sp., Padina sp., and Laurencia sp. Macroalgae Using Green Technology: Effect of In Vitro Digestion on Antioxidant and ACE-I Inhibitory Activity',
+	authors: 'Eda Şensu, Eda Nur Ayar, Emine Şükran Okudan, Beraat Özçelik, Aysun Yücetepe',
+	journal: 'ACS Omega',
+	time: 'December 2023',
+	link: 'https://doi.org/10.1021/acsomega.3c05041'
+},
+
+{
+	title: 'A comparative study on the encapsulation of black carrot extract in potato protein–pectin complexes',
+	authors: 'Merve Yavuz-Düzgün, Eda Şensu, Ümit Altuntaş, Mine Gültekin-Özgüven, Beraat Özçelik',
+	journal: 'Journal of Food Science and Technology',
+	time: '2023',
+	link: 'https://doi.org/10.1007/s13197-023-05787-z'
+},
+
+{
+	title: 'Investigation of changes in some bioactive properties of phenolic extracts from pulp and seed tissues of ziziphus jujuba during in vitro digestion',
+	authors: 'Eda Şensu, Kadriye Nur Kasapoğlu, Sibel Ayşın, Beraat Özçelik',
+	journal: 'GIDA - The Journal of Food',
+	time: 'May 2023',
+	link: 'https://doi.org/10.15237/gida.GD23033'
+},
+
+{
+	title: 'Ultrasound‐assisted enzymatic extraction of proteins from Gracilaria dura: Investigation of antioxidant activity and techno‐functional properties',
+	authors: 'Ayşegül Bozdemir, Eda Şensu, Mine Gültekin-Özgüven, Emine Şükran Okudan, Aysun Yücetepe',
+	journal: 'Journal of Food Processing and Preservation',
+	time: 'June 2022',
+	link: 'https://doi.org/10.1111/jfpp.16803'
+},
+
+{
+	title: 'Optimization of enzymatic hydrolysis degree of Sargassum vulgare protein using response surface methodology',
+	authors: 'Eda Şensu, Hatice Dinç, Emine Şükran Okudan, Aysun Yücetepe',
+	journal: 'GIDA - The Journal of Food',
+	time: '2022',
+	link: 'https://doi.org/10.15237/gida.gd22026'
+},
+
+{
+	title: 'Orange, red and purple barberries: Effect of in-vitro digestion on antioxidants and ACE inhibitors',
+	authors: 'Eda Şensu, Kadriye Nur Kasapoğlu, Mine Gültekin-Özgüven, Evren Demircan, Ayla Arslaner, Beraat Özçelik',
+	journal: 'LWT-Food Science and Technology',
+	time: 'April 2021',
+	link: 'https://doi.org/10.1016/j.lwt.2020.110820'
+},
+
+{
+	title: 'The impact of pH and biopolymer ratio on the complex coacervation of Spirulina platensis protein concentrate with chitosan',
+	authors: 'Aysun Yücetepe, Merve Yavuz-Düzgün, Eda Şensu, Fatih Bildik, Evren Demircan, Beraat Özçelik',
+	journal: 'Journal of Food Science and Technology',
+	time: 'April 2021',
+	link: 'https://doi.org/10.1007/s13197-020-04636-7'
+},
+
+{
+	title: 'BERBERIS PLANTS WITH ORANGE, RED AND PURPLE FRUITS: ANTIOXIDANT AND ANTI-HYPERTANSIVE ACTIVIES',
+	authors: 'Eda Şensu, Beraat Özçelik',
+	journal: 'GIDA - The Journal of Food',
+	time: 'February 2020',
+	link: 'https://doi.org/10.15237/gida.gd20005'
+},
 ];
